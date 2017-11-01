dff1231965844ee351e8f540e76afdf3a201fdf4
DeyuGoGo <a0924xxx@gmail.com>
2017-11-01 14:42:16 +0800

    [marry] try

--- line-echo-bot.js
+++ line-echo-bot.js
@@ -2,14 +2,18 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var request = require('request');
+var EVENT_NAME = '穎奇婚禮';
 var LINE_API_REPLY = "https://api.line.me/v2/bot/message/reply";
-var YOUR_CHANNEL_ACCESS_TOKEN = process.env.CHANNEL_ACCESS_TOKEN ;//your acccess token
-var CHANNEL_ACCESS_TOKEN = "Bearer " + YOUR_CHANNEL_ACCESS_TOKEN ;
+var URL_FCM_MEESSAGE = "https://fcm.googleapis.com/fcm/send";
+var URL_GET_TOKEN = "https://fcm.googleapis.com/fcm/send";
+// var YOUR_CHANNEL_ACCESS_TOKEN = process.env.CHANNEL_ACCESS_TOKEN ;//your acccess token
+// var CHANNEL_ACCESS_TOKEN = "Bearer " + YOUR_CHANNEL_ACCESS_TOKEN ;
+var FCM_API_KEY = "key=AIzaSyDJluA6H2Zlx1B6qEqEFQJ3GBTI4E4s5mE";
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json())
 
-app.post('/linego', (req, res) => {
+app.post('/postBarrge', (req, res) => {
   console.log(req.body);
   res.send('Hello World')
   const result = req.body.events;
@@ -17,29 +21,44 @@ app.post('/linego', (req, res) => {
     const type = result[i]['type'];
     console.log('receive: ', type);
     if(type==='message'){
-      sendTextMessage(result[i].message.text, result[i]['replyToken']);
+      getToken(result[i].message.text)
+      // sendFcm(, result[i]['replyToken']);
     }
   }
 });
 
-function sendTextMessage(text , token) {
-  console.log(text);
-  console.log(token);
-  const data = {
-    replyToken: token,
-    messages: [{
-    "type": "text",
-    "text": text
-  }]
-};
-  request({
-    url: LINE_API_REPLY,
+function getToken(message){
+  const payload = {
+    uid: EVENT_NAME
+  };
+  request(url: FCM_MEESSAGE,
     headers: {
       'Content-Type': 'application/json; charset=UTF-8',
-      'Authorization': CHANNEL_ACCESS_TOKEN
     },
     method: 'POST',
-    body: JSON.stringify(data)
+    body: JSON.stringify(payload)
+  },function(error , response , body){
+    sendFcm(body.token,message)
+    console.log(body);
+  });
+}
+
+function sendFcm(to,message){
+  console.log(to);
+  const payload = {
+    to: to,
+    data: {
+      message: message
+    }
+  };
+  request({
+    url: FCM_MEESSAGE,
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Authorization': FCM_API_KEY
+    },
+    method: 'POST',
+    body: JSON.stringify(payload)
   }, function(error, response, body) {
     if (error) {
       console.log('Error sending message: ', error);
@@ -50,6 +69,35 @@ function sendTextMessage(text , token) {
   });
 }
 
+
+// function sendTextMessage(text , token) {
+//   console.log(text);
+//   console.log(token);
+//   const data = {
+//     replyToken: token,
+//     messages: [{
+//     "type": "text",
+//     "text": text
+//   }]
+// };
+//   request({
+//     url: LINE_API_REPLY,
+//     headers: {
+//       'Content-Type': 'application/json; charset=UTF-8',
+//       'Authorization': CHANNEL_ACCESS_TOKEN
+//     },
+//     method: 'POST',
+//     body: JSON.stringify(data)
+//   }, function(error, response, body) {
+//     if (error) {
+//       console.log('Error sending message: ', error);
+//     } else if (response.body.error) {
+//       console.log('Error: ', response.body.error);
+//     }
+//     console.log('send response: ', body);
+//   });
+// }
+//
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
